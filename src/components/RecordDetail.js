50f1a00e790168640d23d0a7cdaf2c21b6321a5f
bblack1225 <black60137@gmail.com>
2021-09-23 15:33:20 +0800

    feat: init project

---
+++ src/components/RecordDetail.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+
+export default function RecordDetail() {
+  return (
+    <Box
+      component="div"
+      sx={{
+        p: 2,
+        minHeight: 500,
+        width: { xs: "50%", md: "30%" },
+      }}
+    >
+      <Card>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            盈咖哩飯專門
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            10690台北市大安區市民大道四段138-3號
+          </Typography>
+          <Box
+            sx={{
+              width: 200,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Box>昱:</Box>
+            <Rating name="read-only" value={4.5} readOnly />
+          </Box>
+          <Box
+            sx={{
+              width: 200,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Box>俞:</Box>
+            <Rating name="read-only" value={4.5} readOnly />
+          </Box>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+}
---
+++ src/components/Records.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: "center",
+  height: 60,
+  lineHeight: "60px",
+  marginBottom: 10,
+  borderRadius: "0.5rem",
+  boxShadow: "0.1rem 0.1rem 0.5rem rgba(0, 0, 0, 0.171)",
+  paddingTop: 5,
+  paddingBottom: 5,
+}));
+
+const Title = styled(Typography)(({ theme }) => ({
+  ...theme.typography.body2,
+  fontWeight: 700,
+}));
+
+export default function Records() {
+  return (
+    <Box
+      component="div"
+      sx={{
+        p: 2,
+        borderRight: "0.2rem solid rgba(58, 58, 58, 0.548)",
+        minHeight: 500,
+        width: { xs: "50%", md: "30%" },
+        textAlign: "center",
+      }}
+    >
+      {/* {[0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => (
+        <Item key={elevation}>{`elevation=${elevation}`}</Item>
+      ))} */}
+      <Item>
+        <Title component="legend">盈咖哩飯專門</Title>
+        <Rating
+          name="read-only"
+          value={4.5}
+          readOnly
+          precision={0.5}
+          size="small"
+        />
+      </Item>
+      <Item>
+        <Title component="legend">豆花鄉</Title>
+        <Rating name="read-only" value={4} readOnly size="small" />
+      </Item>
+      <Item>
+        <Title component="legend">和美冰果室</Title>
+        <Rating name="read-only" value={5} readOnly size="small" />
+      </Item>
+    </Box>
+  );
+}
